3e55df27d62e3ec6b98a57c1907b6c4c063b5806
Gravity459 <59308294+Gravity459@users.noreply.github.com>
2021-09-01 15:31:24 +0500

    First Commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Tribute Page Project
+
+This is the Tribute Page Project assigned by the Free Code Camp HTML/CSS Course.
+
+This project is one of the starting projects for me and it can help any beginner in pursuit of starting with HTML/CSS.
+
+The live preview for the site is at: https://gravity459.github.io/Tribute-Page/
+
+
+# Calculator - The Odin Project
+
+This project is made on the rules provided by The Odin Project's Fundamental Course.
+
+This Calculator might has some bugs and hidden cases. 
---
+++ script.js
@@ -0,0 +1,340 @@
+
+// global variables
+let displayCalc = '';
+let currentOperator = '';
+let num1 = 0;
+let num2 = 0;
+let floatFlag = 0;
+let num1Flag = 0;
+let num2Flag = 0;
+let result = 0;
+let equalFlag = 0;
+
+// for displaying the previous calculation
+const resultDisplay = document.querySelector('#result-display');
+
+// functions for the operations
+function add()
+{  
+    return num1 + num2;
+}
+
+function sub()
+{  
+    return num1 - num2;
+}
+
+function mul()
+{  
+    return num1 * num2;
+}
+
+function div()
+{  
+    return Math.floor(num1 / num2);
+}
+
+function operate()
+{
+    if( currentOperator == '+')
+    {
+        result = add(num1, num2);
+    }
+    
+    else if( currentOperator == '-')
+    {
+        result = sub(num1, num2);
+    }
+
+    else if( currentOperator == '*')
+    {
+        result = mul(num1, num2);
+    }
+
+    else if( currentOperator == '/')
+    {
+        if(num2 == 0)
+        {
+            alert('Sneaky Sneaky!!!');
+            return 0;
+        }
+        else
+        {
+            result = div(num1, num2);
+        }
+    }
+
+    resultDisplay.textContent = `${num1} ${currentOperator} ${num2} = ${result}`;
+    currentOperator = '';
+}
+
+// for evaluating the respective button
+function valueCalled(e) {
+    
+    // code Clear the display
+    if(e.target.value == 'C')
+    {
+        changeDisplayContent('');
+        changeDisplayContent('0');
+        resetFlags();
+        resultDisplay.textContent = '';
+        return;
+    }
+
+    // for removing the last digit
+    if(e.target.value == 'D')
+    {
+        changeDisplayContent('D');
+        return;
+    }
+    
+    // for handling the 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    if(e.target.value == '.')
+    {
+        if(!floatFlag)
+        {
+            floatFlag = 1;
+            changeDisplayContent('.');
+            return;
+        }
+        else 
+        {
+            return;
+        }
+    }
+
+    // if the operator is not '='
+    if(e.target.classList.contains('operator') && e.target.value != '=')
+    {
+        // console.log('watch out he has an op : ' + e.target.value);
+        // console.log(currentOperator);
+
+        if(equalFlag && e.target.classList.contains('operator'))
+        {
+            currentOperator = e.target.value;
+            equalFlag = 0;
+            return;
+        }
+
+        // if num1 is empty then load the entered number in num1 
+        // else put the num1flag on and enter it in num2
+
+        if(!num1Flag)
+        {   
+            currentOperator = e.target.value;
+            if(checkFloat())
+            {
+                changeNum1(parseFloat(displayCalc));
+            }
+
+            else 
+            {
+                changeNum1(parseInt(displayCalc));
+            }
+
+            // console.log('the number was saved in num1');
+            // console.log(num1);
+            changeDisplayContent('');
+        }
+        
+        else if(!num2Flag)
+        {
+            if(displayCalc == '')
+            {
+                currentOperator = e.target.value;
+                return;
+            }
+
+            if(checkFloat())
+            {
+                changeNum2(parseFloat(displayCalc));
+            }
+            else 
+            {
+                changeNum2(parseInt(displayCalc));
+            }
+            
+            // console.log('the number was saved in num2 : ' + num2);
+            operate();
+            changeDisplayContent('');
+            changeDisplayContent('result');
+            changeNum1(result);
+            changeNum2(0);
+            num2Flag = 0;
+            currentOperator = e.target.value;
+        }
+
+        return;
+    }
+    
+    // if the operator is '='
+    else if(e.target.value == '=')
+    {
+        // console.log('here');
+        if(num1Flag && (displayCalc != '0' || displayCalc != '') && currentOperator != '')
+        {
+            if(checkFloat())
+            {
+                changeNum2(parseFloat(displayCalc));
+            }
+            else 
+            {
+                changeNum2(parseInt(displayCalc));
+            }
+
+            operate();
+            changeDisplayContent('');
+            changeDisplayContent('result');
+            displayCalc = '0';
+            changeNum1(result);
+            changeNum2(0);
+            num2Flag = 0;
+            currentOperator = '';
+            equalFlag = 1;
+        }
+        return;
+    }
+    
+
+    // if it is a value then display it on the screen
+    if(e.target.classList.contains('number'))
+    {
+        if(equalFlag)
+        {
+            changeNum1(0);
+            num1Flag = 0;
+            equalFlag = 0;
+        }
+        changeDisplayContent(e.target.value);
+    }
+    
+}
+
+function checkFloat()
+{
+    if(floatFlag)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+function changeNum1(num) {
+    num1 = num;
+    // console.log('num1 was modified to : ' + num);
+    
+    num1Flag = 1;
+    // console.log('num1 flag is now : ' + num1Flag);
+    floatFlag = 0;
+    displayCalc = '';
+}
+
+function changeNum2(num) {
+    num2 = num;
+    // console.log('num2 was modified to : ' + num);
+    
+    num2Flag = 1;
+    // console.log('num2 flag is now : ' + num2Flag);
+    floatFlag = 0;
+}
+
+
+function resetFlags(){
+    displayCalc = '0';
+    currentOperator = '';
+    num1 = 0;
+    num2 = 0;
+    floatFlag = 0;
+    num1Flag = 0;
+    num2Flag = 0;
+    equalFlag = 0;
+}
+
+
+function changeDisplayContent(value) {
+
+    if(value == '')
+    {
+        displayCalc = '';
+        // console.log('display was cleared');
+    }
+    else if(value == 'D')
+    {
+        console.log(displayCalc);
+        if(displayCalc.length == 1)
+        {
+            displayCalc = '0';
+        }
+        else
+        {
+            displayCalc = displayCalc.slice(0, -1);
+        }
+        // console.log('a number was deleted');
+    }
+    else if(value == 'result')
+    {
+        displayCalc = result;
+        // console.log('result logged');
+    }
+    else
+    {
+        if(displayCalc.length == 1 && displayCalc == '0')
+        {
+            displayCalc = value;
+        }
+        else
+        {
+            displayCalc += value;
+        }
+        // console.log('number added to display : ' + value);
+    }
+    
+    const displaySpan = document.querySelector('#displayCalc');
+    // console.log(displaySpan);
+    
+    displaySpan.textContent = displayCalc;
+}
+
+function calcStart()
+{
+    const buttons = document.querySelectorAll('.operator');
+    // console.log(buttons);
+    
+    const numbers = document.querySelectorAll('.number');
+    // console.log(numbers);
+    
+    numbers.forEach(number => number.addEventListener('click', valueCalled));
+    buttons.forEach(button => button.addEventListener('click', valueCalled));
+    
+    changeDisplayContent('0');
+}
+
+calcStart();
